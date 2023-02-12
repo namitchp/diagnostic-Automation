651fedc3a613f8b1f6c4e3ef0981500179d727f8
Mohit <mohit@rohininfo.com>
2023-02-12 15:17:06 +0530

    minor header changes

--- src/components/main_menu.js
+++ src/components/main_menu.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
 const MainMenu = (props) => {
-    const { list, onMenuChange } = props;
-    const selectedMenu = window.location.pathname.split("/")[1];
-    return(
-        <div className="header-bottom">
-                <div className="px-3">
-                    <div className="header-navs header-navs-left">
-                    <ul className="menu-nav mb-0 list-unstyled d-flex  flex-wrap">
-                            {list.length > 0 ? list?.map(( item, index ) => {
-                                return <li className={"menu-item rounded  px-4 mr-2  menu-level1-color "+ (selectedMenu === (item.transaction_name.replace(/\s+/g, '-').toLowerCase()) ? "menu-level1-bg" :"")} key={"menulist" + index}>
-                                    <Link onClick={() => onMenuChange(item.transaction_id,index)} className={"menu-link py-2 px-4 rounded  d-inline-block "+ (selectedMenu === (item.transaction_name.replace(/\s+/g, '-').toLowerCase()) ? " text-dark" : "text-white") } to={`/${item.transaction_name.toLowerCase()}/${item.transaction_id}`}> <span className="">{item.display_name}</span></Link>
-                                </li>
-                            }) : null}
-                     </ul>
-                </div>
-            </div>
+  const { list, onMenuChange } = props;
+  const selectedMenu = window.location.pathname.split("/")[1];
+  return (
+    <div className="header_menu_wrapper">
+      <Container fluid>
+        <div className="header-navs header-navs-left">
+          <ul className="menu-nav mb-0 list-unstyled d-flex  flex-wrap">
+            {list.length > 0
+              ? list?.map((item, index) => {
+                  return (
+                    <li
+                      className={
+                        "menu-item rounded menu-level1-color " +
+                        (selectedMenu ===
+                        item.transaction_name.replace(/\s+/g, "-").toLowerCase()
+                          ? "menu-level1-bg"
+                          : "")
+                      }
+                      key={"menulist" + index}
+                    >
+                      <Link
+                        onClick={() => onMenuChange(item.transaction_id, index)}
+                        className={
+                          "menu-link d-inline-block " +
+                          (selectedMenu ===
+                          item.transaction_name
+                            .replace(/\s+/g, "-")
+                            .toLowerCase()
+                            ? " text-dark"
+                            : "text-white")
+                        }
+                        to={`/${item.transaction_name.toLowerCase()}/${
+                          item.transaction_id
+                        }`}
+                      >
+                        {" "}
+                        <span className="">{item.display_name}</span>
+                      </Link>
+                    </li>
+                  );
+                })
+              : null}
+          </ul>
         </div>
-    )
-}
+      </Container>
+    </div>
+  );
+};
 
 export default MainMenu;
 
